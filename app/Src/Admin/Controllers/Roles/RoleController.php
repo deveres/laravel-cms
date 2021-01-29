efc56fa0e334c10905a603945c2d3ee81fa1611c
DeVeres <vadimbirsan@gmail.com>
2021-01-29 12:56:14 +0300

    Fix roles grid display

---
+++ app/Src/Admin/Controllers/Roles/RoleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Src\Admin\Controllers\Roles;
+
+
+use Encore\Admin\Grid;
+
+
+class RoleController extends \Encore\Admin\Controllers\RoleController
+{
+
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $roleModel = config('admin.database.roles_model');
+
+        $grid = new Grid(new $roleModel());
+
+        $grid->column('id', 'ID')->sortable();
+        $grid->column('slug', trans('admin.slug'));
+        $grid->column('name', trans('admin.name'));
+
+        $grid->column('permissions', trans('admin.permission'))->pluck('name')->customLabel();
+
+        $grid->column('created_at', trans('admin.created_at'));
+        $grid->column('updated_at', trans('admin.updated_at'));
+
+        $grid->actions(function (Grid\Displayers\Actions $actions) {
+            if ($actions->row->slug == 'administrator') {
+                $actions->disableDelete();
+            }
+        });
+
+        $grid->tools(function (Grid\Tools $tools) {
+            $tools->batch(function (Grid\Tools\BatchActions $actions) {
+                $actions->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+
+
+}
